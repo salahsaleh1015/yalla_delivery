26f202d89c1d1ba22b1590613bc1a965895831f5
Salah Saleh <salahsaleh1015@gmail.com>
2024-12-08 19:41:13 +0200

    create Confirm order Dialog

--- lib/features/home/view/summary_view.dart
+++ lib/features/home/view/summary_view.dart
@@ -1,9 +1,11 @@
 import 'package:delivery_app/features/global_widgets/global_app_bar.dart';
 import 'package:delivery_app/features/global_widgets/global_button_widget.dart';
+import 'package:delivery_app/features/global_widgets/global_dialog.dart';
 import 'package:delivery_app/features/home/widgets/items/edit_location_card_item_widget.dart';
 import 'package:delivery_app/features/home/widgets/items/notes_section_item_widget.dart';
 import 'package:delivery_app/features/home/widgets/items/summary_delivery_item_widget.dart';
 import 'package:delivery_app/features/home/widgets/items/summary_details_item_widget.dart';
+import 'package:delivery_app/features/main_layout/views/main_layout_view.dart';
 import 'package:delivery_app/resources/colors_manager.dart';
 import 'package:delivery_app/resources/values_manager.dart';
 import 'package:flutter/material.dart';
@@ -60,9 +62,33 @@ class SummaryView extends StatelessWidget {
       ),
       bottomNavigationBar: GlobalButtonWidget(
         text: "تأكيد الطلب",
-        onTap: () {},
+        onTap: () {
+          showCustomDialog(
+            context,
+            content: const ConfirmOrderDialogContent(),
+            actionButtonCallBack: () {
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                MainLayoutView.id,
+                    (Route<dynamic> route) => false,
+              );
+            },
+            actionButtonHint: "الرئيسية",
+            dialogTitle: "تغيير موقع التوصيل",
+          );
+        },
         width: double.infinity,
       ),
     );
+  }
+}
+
+class ConfirmOrderDialogContent extends StatelessWidget {
+  const ConfirmOrderDialogContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("سنراجع طلبك ونقبله في أسرع وقت ممكن.",
+      style: Theme.of(context).textTheme.headlineSmall,);
   }
 }
--- lib/features/home/widgets/items/edit_location_card_item_widget.dart
+++ lib/features/home/widgets/items/edit_location_card_item_widget.dart
@@ -64,42 +64,52 @@ class EditLocationCardItemWidget extends StatelessWidget {
         actionButtonCallBack: () {},
         actionButtonHint: "تغيير",
         dialogTitle: "تغيير موقع التوصيل",
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        content: const LocationDialogContent());
+  }
+}
+
+class LocationDialogContent extends StatelessWidget {
+  const LocationDialogContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
           children: [
-            Row(
-              children: [
-                Text(
-                  "موقع التوصيل: ",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                SizedBox(
-                    width:
-                        MediaQuery.of(context).size.width * 0.4,
-                    child: Text("مدينة 6 أكتوبر، محافظة الجيزة",
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyMedium)),
-              ],
-            ),
-            SizedBox(
-              height: AppSize.s20.h,
-            ),
             Text(
-              "موقعك الان بالتفصيل",
+              "موقع التوصيل: ",
               style: Theme.of(context).textTheme.bodyMedium,
             ),
             SizedBox(
-              height: AppSize.s20.h,
-            ),
-            GlobalTextFieldWidget(
-              height: AppSize.s100.h,
-              hintText: "أدخل موقعك الان بالتفصيل",
-              textInputType: TextInputType.multiline,
-            ),
+                width:
+                MediaQuery.of(context).size.width * 0.4,
+                child: Text("مدينة 6 أكتوبر، محافظة الجيزة",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium)),
           ],
-        ));
+        ),
+        SizedBox(
+          height: AppSize.s20.h,
+        ),
+        Text(
+          "موقعك الان بالتفصيل",
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        SizedBox(
+          height: AppSize.s20.h,
+        ),
+        GlobalTextFieldWidget(
+          height: AppSize.s100.h,
+          hintText: "أدخل موقعك الان بالتفصيل",
+          textInputType: TextInputType.multiline,
+        ),
+      ],
+    );
   }
 }
+
