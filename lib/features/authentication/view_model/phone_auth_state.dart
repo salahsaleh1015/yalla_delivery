7f343fcae4a688112c01cc2e022ccab9af43b826
Salah Saleh <salahsaleh1015@gmail.com>
2025-05-18 19:16:07 +0300

    add phone auth states

---
+++ lib/features/authentication/view_model/phone_auth_state.dart
@@ -0,0 +1,18 @@
+part of 'phone_auth_cubit.dart';
+
+@immutable
+class PhoneAuthState {}
+
+final class PhoneAuthInitial extends PhoneAuthState {}
+
+final class PhoneAuthLoading extends PhoneAuthState {}
+
+final class PhoneAuthError extends PhoneAuthState {
+  final String errorMsg;
+
+  PhoneAuthError({required this.errorMsg});
+}
+
+final class PhoneAuthNumberSubmitted extends PhoneAuthState {}
+
+final class PhoneAuthCodeVerified extends PhoneAuthState {}
