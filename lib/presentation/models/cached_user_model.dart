8853d4080df3776b807ea4dcf8ce6c791c85c07e
Salah Saleh <salahsaleh1015@gmail.com>
2025-10-06 15:32:08 +0300

    ..

---
+++ lib/presentation/models/cached_user_model.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
+
+part 'cached_user_model.freezed.dart';
+part 'cached_user_model.g.dart';
+
+@freezed
+@HiveType(typeId: 0) // Hive adapter type ID
+class CachedUserModel with _$CachedUserModel {
+  const factory CachedUserModel({
+    @HiveField(0) required String userLocation,
+    @HiveField(1) required String userName,
+    @HiveField(2) required String phoneNumber,
+    @HiveField(3) String? userId,
+  }) = _CachedUserModel;
+
+  factory CachedUserModel.fromJson(Map<String, dynamic> json) =>
+      _$CachedUserModelFromJson(json);
+}
