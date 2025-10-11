48863d2e00a876fbe495802c884f5e1ed2f1e116
Salah Saleh <salahsaleh1015@gmail.com>
2025-10-11 17:55:02 +0300

    Delivery Order Card

--- lib/presentation/views/delivery_views/views/delivery_home/views/delivery_home_view.dart
+++ lib/presentation/views/delivery_views/views/delivery_home/views/delivery_home_view.dart
@@ -1,12 +1,6 @@
 import 'package:delivery_app/core/resources/assets_manager.dart';
-import 'package:delivery_app/core/resources/colors_manager.dart';
 import 'package:delivery_app/core/resources/values_manager.dart';
-import 'package:delivery_app/presentation/views/admin_views/views/admin_delivery_management/widgets/delivery_status_badge.dart';
-import 'package:delivery_app/presentation/views/global_widgets/global_button_widget.dart';
-import 'package:delivery_app/presentation/views/global_widgets/global_decorated_container.dart';
-import 'package:delivery_app/presentation/views/global_widgets/global_light_button_widget.dart';
 import 'package:delivery_app/presentation/views/global_widgets/global_padding_widget.dart';
-import 'package:delivery_app/presentation/views/global_widgets/global_view_all_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -53,103 +47,13 @@ class DeliveryHomeView extends StatelessWidget {
             SizedBox(
               height: AppSize.s25.h,
             ),
-            GlobalDecoratedContainer(
-                padding: EdgeInsets.symmetric(
-                    horizontal: AppSize.s15.w, vertical: AppSize.s25.h),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        buildOrderInfoStatement(
-                          context: context,
-                          title: "رقم الطلب: ",
-                          value: "50#",
-                        ),
-                        const Spacer(),
-                        DeliveryStatusBadge(
-                            statusColor: ColorManager.lightGrayColor,
-                            deliveryStatus: "معلقة",
-                            deliveryStatusColor: ColorManager.darkGrayColor),
-                      ],
-                    ),
-                    SizedBox(
-                      height: AppSize.s5.h,
-                    ),
-                    buildOrderInfoStatement(
-                      title: "المستخدم: ",
-                      value: "محمد علي",
-                      context: context,
-                    ),
-                    SizedBox(
-                      height: AppSize.s12.h,
-                    ),
-                    buildOrderInfoStatement(
-                      title: "العنوان: ",
-                      value: "مدينة 6 أكتوبر, محافظة الجيزة",
-                      context: context,
-                    ),
-                    SizedBox(
-                      height: AppSize.s12.h,
-                    ),
-                    buildOrderInfoStatement(
-                      title: "الوقت: ",
-                      value: "03:00 م",
-                      context: context,
-                    ),
-                    SizedBox(
-                      height: AppSize.s25.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        GlobalLightButtonWidget(
-                          onTap: () {},
-                          width: MediaQuery.of(context).size.width * 0.41,
-                          height: AppSize.s33.h,
-                          child: Text(
-                            "حذف",
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge!
-                                .copyWith(color: ColorManager.error),
-                          ),
-                        ),
-                        GlobalButtonWidget(
-                          isButtonEnabled: true,
-                          text: "قبول",
-                          onTap: () {},
-                          width: MediaQuery.of(context).size.width * 0.41,
-                          height: AppSize.s33.h,
-                        ),
-                      ],
-                    )
-                  ],
-                )),
+
           ],
         ),
       ),
     );
   }
 
-  Widget buildOrderInfoStatement(
-          {required BuildContext context,
-          required String title,
-          required String value}) =>
-      Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: Theme.of(context)
-                .textTheme
-                .headlineMedium!
-                .copyWith(color: ColorManager.darkGrayColor),
-          ),
-          Text(
-            value,
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ],
-      );
+
 }
+
---
+++ lib/presentation/views/delivery_views/views/delivery_home/widgets/delivery_order_card.dart
@@ -0,0 +1,108 @@
+import 'package:delivery_app/presentation/views/global_widgets/global_button_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../../core/resources/colors_manager.dart';
+import '../../../../../../core/resources/values_manager.dart';
+import '../../../../admin_views/views/admin_delivery_management/widgets/delivery_status_badge.dart';
+import '../../../../global_widgets/global_decorated_container.dart';
+import '../../../../global_widgets/global_light_button_widget.dart';
+class DeliveryOrderCard extends StatelessWidget {
+  const DeliveryOrderCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GlobalDecoratedContainer(
+        padding: EdgeInsets.symmetric(
+            horizontal: AppSize.s15.w, vertical: AppSize.s25.h),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                buildOrderInfoStatement(
+                  context: context,
+                  title: "رقم الطلب: ",
+                  value: "50#",
+                ),
+                const Spacer(),
+                DeliveryStatusBadge(
+                    statusColor: ColorManager.lightGrayColor,
+                    deliveryStatus: "معلقة",
+                    deliveryStatusColor: ColorManager.darkGrayColor),
+              ],
+            ),
+            SizedBox(
+              height: AppSize.s5.h,
+            ),
+            buildOrderInfoStatement(
+              title: "المستخدم: ",
+              value: "محمد علي",
+              context: context,
+            ),
+            SizedBox(
+              height: AppSize.s12.h,
+            ),
+            buildOrderInfoStatement(
+              title: "العنوان: ",
+              value: "مدينة 6 أكتوبر, محافظة الجيزة",
+              context: context,
+            ),
+            SizedBox(
+              height: AppSize.s12.h,
+            ),
+            buildOrderInfoStatement(
+              title: "الوقت: ",
+              value: "03:00 م",
+              context: context,
+            ),
+            SizedBox(
+              height: AppSize.s25.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GlobalLightButtonWidget(
+                  onTap: () {},
+                  width: MediaQuery.of(context).size.width * 0.41,
+                  height: AppSize.s33.h,
+                  child: Text(
+                    "حذف",
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(color: ColorManager.error),
+                  ),
+                ),
+                GlobalButtonWidget(
+                  isButtonEnabled: true,
+                  text: "قبول",
+                  onTap: () {},
+                  width: MediaQuery.of(context).size.width * 0.41,
+                  height: AppSize.s33.h,
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+  Widget buildOrderInfoStatement(
+      {required BuildContext context,
+        required String title,
+        required String value}) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium!
+                .copyWith(color: ColorManager.darkGrayColor),
+          ),
+          Text(
+            value,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      );
+}
