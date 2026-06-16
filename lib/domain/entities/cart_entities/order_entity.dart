96e297be5ce185d62f268409e18e089015a74c8d
Salah Saleh <salahsaleh1015@gmail.com>
2026-06-16 19:30:50 +0300

    add user order entity

--- lib/domain/entities/cart_entities/order_entity.dart
+++ lib/domain/entities/cart_entities/order_entity.dart
@@ -1,12 +1,36 @@
-class OrderEntity {
-  final String orderDetails;
-  final String orderStatus;
-  final String orderNotes;
-  final String orderDate;
+import 'package:hive/hive.dart';
 
-  OrderEntity(
-      {required this.orderDetails,
-      required this.orderStatus,
-      required this.orderNotes,
-      required this.orderDate});
+part 'order_entity.g.dart';
+
+@HiveType(typeId: 5)
+class OrderEntity {
+  @HiveField(0)
+  final String userName;
+  @HiveField(1)
+  final String userLocation;
+  @HiveField(2)
+  final String userPhone;
+  @HiveField(3)
+  final String deliveryName;
+  @HiveField(4)
+  final String orderDetails;
+  @HiveField(5)
+  final String orderStatus;
+  @HiveField(6)
+  final String orderRequestTime;
+  @HiveField(7)
+  final String orderNotes;
+  @HiveField(8)
+  final String orderId;
+
+ OrderEntity(
+      {required this.userName,
+        required this.orderId,
+        required this.userLocation,
+        required this.userPhone,
+        required this.deliveryName,
+        required this.orderDetails,
+        required this.orderStatus,
+        required this.orderRequestTime,
+        required this.orderNotes});
 }
